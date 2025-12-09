c0a0939e337644ba5f2e2e9bc0a15227134491ab
nufuth <onlinead1111@gmail.com>
2025-12-09 10:54:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import { Salla } from '@salla.sa/cli';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,15 +7,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Lavista App is Running! 🚀');
+  res.send('Lavista App is Working! 🚀');
 });
 
-// هذا الرابط الذي ستضعه في سلة (Callback URL)
-app.get('/callback', async (req, res) => {
-  res.send('تم ربط التطبيق بنجاح! يمكنك إغلاق الصفحة.');
-  console.log("تم استدعاء الكول باك");
+app.get('/callback', (req, res) => {
+  res.send('تم الاتصال بنجاح!');
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
